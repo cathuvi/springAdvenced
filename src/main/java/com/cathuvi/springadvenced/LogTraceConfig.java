6947203b7e46e77ee8eb70a34dce4f0fe1a9d88e
ts.youn <huvi.media@gmail.com>
2023-05-23 18:43:43 +0900

    스프링 bean을 이용한 주입

--- src/main/java/com/cathuvi/springadvenced/LogTraceConfig.java
+++ src/main/java/com/cathuvi/springadvenced/LogTraceConfig.java
@@ -2,6 +2,7 @@ package com.cathuvi.springadvenced;
 
 import com.cathuvi.springadvenced.trace.logtrace.FiledLogTrace;
 import com.cathuvi.springadvenced.trace.logtrace.LogTrace;
+import com.cathuvi.springadvenced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace(){
-        return new FiledLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
